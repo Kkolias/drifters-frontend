84952bc60c1bd73ce1b2a90dc3e9d90be660487c
Elias Koivuporras <eppu2002@gmail.com>
2024-04-30 20:55:38 +0300

    optimization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,21 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   css: ["~/assets/less/main.less"],
+
+  // default title for all pages
+  app: {
+    head: {
+      title: "DrifDataan - Drifting tilastot ja tulokset",
+      meta: [
+        // default meta tags:
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content:
+            "DriftDataan tarjoaa kattavat tilastot ja tulokset Drift Masters Europe sarjasta. Seuraa driftaustapahtumien kuumimpia hetkiä ja tutustu huippuluokan driftaajiin!",
+        },
+      ],
+    },
+  },
 });
--- pages/series/index.vue
+++ pages/series/index.vue
@@ -1,6 +1,21 @@
 <template>
-    <div class="page-series">
-        <DriftSeriesPage  />
-    </div>
+  <div class="page-series">
+    <DriftSeriesPage />
+  </div>
 </template>
 
+<script setup lang="ts">
+const title = ref("DriftDataan - Driftisarjat");
+const description = ref("DriftDataan - Driftisarjat ja kaudet");
+
+// This will be reactive when you change title/description above
+useHead({
+  title,
+  meta: [
+    {
+      name: "description",
+      content: description,
+    },
+  ],
+});
+</script>
