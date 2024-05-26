08ac7e1d3359a85eb344dc3bc2f1e72de6ad1b7d
Elias Koivuporras <eppu2002@gmail.com>
2024-05-26 20:07:28 +0300

    yml configs

--- utils/isProduction.ts
+++ utils/isProduction.ts
@@ -1,9 +1,10 @@
 
 const IS_PRODUCTION = process.env.IS_PRODUCTION === 'true';
+const IS_DEVELOPMENT = process.env.IS_DEVELOPMENT === 'true';
 
 export function isProduction(): boolean {
     if(typeof window === 'undefined') {
-        return IS_PRODUCTION
+        return IS_PRODUCTION || IS_DEVELOPMENT // käytetään nyt alkuun samaa
     }
     return window.location.hostname !== 'localhost'
 }
