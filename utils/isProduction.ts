63c149b54a1a4a8d96fc705b6688c090525e5350
Elias Koivuporras <eppu2002@gmail.com>
2024-05-27 18:57:12 +0300

    debug console.logs

--- utils/isProduction.ts
+++ utils/isProduction.ts
@@ -1,8 +1,11 @@
 
+
 const IS_PRODUCTION = process.env.IS_PRODUCTION === 'true';
 const IS_DEVELOPMENT = process.env.IS_DEVELOPMENT === 'true';
 
 export function isProduction(): boolean {
+    console.log('IS_PRODUCTION', IS_PRODUCTION)
+    console.log('IS_DEVELOPMENT', IS_DEVELOPMENT)
     if(typeof window === 'undefined') {
         return IS_PRODUCTION || IS_DEVELOPMENT // käytetään nyt alkuun samaa
     }
