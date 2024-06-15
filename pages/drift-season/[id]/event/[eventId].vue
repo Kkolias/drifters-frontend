ef11d4923053c17882a0345a0d13832e0398a61b
Elias Koivuporras <eppu2002@gmail.com>
2024-06-15 11:07:22 +0300

    drift event titles

--- pages/drift-season/[id]/event/[eventId].vue
+++ pages/drift-season/[id]/event/[eventId].vue
@@ -1,10 +1,13 @@
 <template>
   <div class="page-drift-event">
-    <DriftSeasonEventDriftEventPage :seasonSlug="seasonSlug" :eventSlug="eventSlug" />
+    <DriftSeasonEventDriftEventPage
+      :seasonSlug="seasonSlug"
+      :eventSlug="eventSlug"
+    />
   </div>
 </template>
 
-<script lang="ts">
+<!-- <script lang="ts">
 export default {
   computed: {
     eventSlug(): string {
@@ -15,4 +18,60 @@ export default {
     },
   },
 };
+</script> -->
+
+<script setup lang="ts">
+const defaultTitle = ref(
+  "Drift SM - Suomen Parasta Driftingiä | Drift SM Pro ja Pro2"
+);
+const defaultDescription = ref(
+  "DriftDataan - Tutustu Drift SM -sarjaan, Suomen arvostetuimpaan drifting-sarjaan. Lue lisää SM Pro- ja SM Pro2-luokista, tapahtumista ja kilpailukalenterista. Seuraa Suomen parhaita drifting-kuljettajia kaudella 2024."
+);
+
+const route = useRoute();
+const eventSlug = computed(() => {
+  return (route?.params?.eventId as string) || "";
+});
+
+const seasonSlug = computed(() => {
+  return (route?.params?.id as string) || "";
+});
+
+const isDriftSm = computed((): boolean => {
+  // if seasonSlug has "drift-sm" in it
+  return seasonSlug?.value?.includes("drift-sm");
+});
+const isDmec = computed((): boolean => {
+  // if seasonSlug has "dmec" in it
+  return seasonSlug?.value?.includes("drift-masters-european-championship");
+});
+
+const title = computed(() => {
+  if (isDriftSm?.value) {
+    return "Drift SM Pro - Lajittelu- ja kaaviotulokset | DriftDataan";
+  } else if (isDmec?.value) {
+    return "Drift Masters European Championship - Lajittelu- ja kaaviotulokset | DriftDataan";
+  }
+  return "DriftDataan - Drifting tulokset ja tilastot";
+});
+
+const description = computed(() => {
+  if (isDriftSm?.value) {
+    return "DriftDataan - Seuraa Drift SM Pro -sarjan kilpailuja ja tuloksia. Lue lisää Drift SM Pro -sarjan kilpailuista, tuloksista ja tilastoista. Seuraa Suomen parhaita drifting-kuljettajia kaudella 2024.";
+  } else if (isDmec?.value) {
+    return "DriftDataan - Seuraa Drift Masters European Championship -sarjan kilpailuja ja tuloksia. Lue lisää Drift Masters European Championship -sarjan kilpailuista, tuloksista ja tilastoista. Seuraa Euroopan parhaita drifting-kuljettajia kaudella 2024.";
+  }
+  return "DriftDataan - Drifting tulokset ja tilastot";
+});
+// This will be reactive when you change title/description above
+useHead({
+  title: title?.value || defaultTitle,
+  meta: [
+    {
+      name: "description",
+      content: description?.value || defaultDescription,
+    },
+  ],
+});
+
 </script>
