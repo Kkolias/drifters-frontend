78db60197ca4e6b3a00bf04d78bc0f8ced102b0a
Elias Koivuporras <eppu2002@gmail.com>
2024-07-29 22:32:26 +0300

    DriverListPage styling

--- components/Drivers/ListPage.vue
+++ components/Drivers/ListPage.vue
@@ -2,15 +2,16 @@
   <div class="component-DriverListPage">
     <div class="header-section">
       <h1>
-        Drifting-Kuljettajat - Tutustu Parhaisiin Kuskeihin ja
-        Heidän Kilpailusarjoihinsa
+        Drifting-Kuljettajat - Tutustu Parhaisiin Kuskeihin ja Heidän
+        Kilpailusarjoihinsa
       </h1>
       <p>
         Drifting-kuljettajasivulta löytyy kattava lista huipputason
-        drifting-kuljettajista ja kuljettajan profiilista helpon esikatselun heidän ajamistaan
-        kilpailusarjoista. Kaikki kuljettajat ajavat joko Drift Masters European
-        Championship -sarjaa, Suomen Drift SM -sarjaa tai molempia. Klikkaa
-        kunkin kuljettajan nimeä saadaksesi lisää tietoa heidän sijoituksistaan kausilla.
+        drifting-kuljettajista ja kuljettajan profiilista helpon esikatselun
+        heidän ajamistaan kilpailusarjoista. Kaikki kuljettajat ajavat joko
+        Drift Masters European Championship -sarjaa, Suomen Drift SM -sarjaa tai
+        molempia. Klikkaa kunkin kuljettajan nimeä saadaksesi lisää tietoa
+        heidän sijoituksistaan kausilla.
       </p>
     </div>
   </div>
@@ -36,6 +37,24 @@
     margin-top: 24px;
     padding: 0 24px;
     text-align: center;
+  }
+
+  @media only screen and (max-width: 600px) {
+    h1 {
+      font-size: 24px;
+    }
+    p {
+      font-size: 14px;
+    }
+  }
+
+  @media only screen and (max-width: 400px) {
+    h1 {
+      font-size: 20px;
+    }
+    p {
+      font-size: 12px;
+    }
   }
 }
 </style>
