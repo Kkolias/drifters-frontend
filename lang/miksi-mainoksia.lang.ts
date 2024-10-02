8be8c8579b7669acc547d35b1e7764a399dedda3
Elias Koivuporras <eppu2002@gmail.com>
2024-10-02 20:14:46 +0300

    miksi mainoksia translations

---
+++ lang/miksi-mainoksia.lang.ts
@@ -0,0 +1,69 @@
+export default {
+    fi: {
+        title: "Miksi mainoksia?",
+        subText1: `Tervetuloa DriftDataan-sivustolle, joka on omistautunut tarjoamaan
+        tarkkaa ja ajankohtaista tietoa drifting-maailmasta suomeksi. Tämä
+        sivusto on drifting-fanin vapaa-ajan projekti, ja minä olen ainoa
+        ylläpitäjä. Pyrin päivittämään sivustoa niin usein kuin mahdollista,
+        mutta huomioithan, että kaiken tiedon päivittäminen voi viedä aikaa.
+        Pistetilastot, taulukot ja muu esitetty data saattavat sisältää virheitä
+        tai olla epätarkkoja, koska tiedon kerääminen ja päivittäminen on
+        käsityötä. Teen parhaani korjatakseni mahdolliset virheet ja
+        tarjotakseni tarkkaa ja luotettavaa tietoa drifting-maailmasta.`,
+        subText2: `Koska sivusto on yksityisesti rahoitettu harrastusprojekti, päätin
+        lisätä mainoksia kattamaan ylläpidosta aiheutuvia kuluja, kuten
+        serverikustannukset, domain-maksut ja muut tekniset kulut. Mainokset
+        auttavat minua pitämään sivuston yllä ja mahdollistavat sen
+        kehittämisen, jotta voin tarjota drifting-yhteisölle entistä parempaa
+        sisältöä.`,
+        section2Title: "Mainokset ja niiden merkitys",
+        section2SubText: `Mainosten tuotto käytetään yksinomaan sivuston ylläpitokustannusten
+        kattamiseen ja sen kehittämiseen. Näin voin varmistaa, että DriftDataan
+        pysyy toiminnassa ja tarjoaa ajankohtaista tietoa drifting-harrastajille
+        ja -faneille. Toivon, että mainokset eivät häiritse liikaa
+        käyttäjäkokemustasi ja että ymmärrät niiden merkityksen sivuston
+        jatkuvuuden kannalta.`,
+        section3Title: "Adblockerien käyttö",
+        section3SubText: `Ymmärrän, että kaikki eivät halua nähdä mainoksia selatessaan
+        internetiä. Jos haluat poistaa mainokset käytöstä, voit käyttää
+        adblockeria. Haluan kuitenkin huomauttaa, että pitämällä mainokset
+        päällä tuet tätä vapaa-ajan projektina toteutettua sivustoa ja autat
+        kattamaan sen ylläpidosta aiheutuvia kuluja. Kiitos ymmärryksestäsi ja
+        tuestasi!`,
+        foundError: `Jos sinulla on kehitysidea tai havaitsit virheen, voit ottaa yhteyttä
+        minuun osoitteessa`,
+        foundError2: `Pyrin
+        vastaamaan viesteihin mahdollisimman pian ja ottamaan kaikki palautteet
+        huomioon sivuston kehittämisessä.`,
+        thankYou: "Kiitos, että käytät DriftDataan-sivustoa!"
+    },
+    en: {
+        title: "Why Ads?",
+        subText1: `Welcome to DriftDataan, website dedicated to providing accurate 
+        and up-to-date information about the world of drifting, available in both Finnish and English. 
+        This site is a personal project created by a drifting enthusiast, 
+        and I am the sole administrator. I strive to update the site as frequently as possible, 
+        but please note that updating all information may take time. Point standings, tables, 
+        and other presented data might contain errors or inaccuracies as all data collection 
+        and updates are done manually. I do my best to fix any mistakes and provide reliable 
+        and precise information about the drifting world.`,
+        subText2: `Since this website is a privately funded hobby project, 
+        I decided to add ads to cover maintenance costs, such as server expenses, 
+        domain fees, and other technical costs. The ads help me keep the site running 
+        and allow for its development so I can continue providing even better content for the drifting community`,
+        section2Title: "Ads and Their Importance",
+        section2SubText: `All ad revenue is used solely to cover the site’s 
+        maintenance costs and its development. This ensures that DriftDataan remains 
+        operational and continues to provide up-to-date information for drifting enthusiasts and fans. 
+        I hope the ads won’t disrupt your user experience too much, 
+        and that you understand their importance for the site's sustainability.`,
+        section3Title: "Using Adblockers",
+        section3SubText: `I understand that not everyone wants to see ads while browsing the internet. 
+        If you prefer to disable ads, you can use an ad blocker. However, I’d like to point out that by keeping ads on, 
+        you support this hobby project and help cover its maintenance costs. Thank you for your understanding and support!`,
+        foundError: `If you have any suggestions or found an error, you can contact me at`,
+        foundError2: `I aim to respond to messages as soon as possible and take all feedback into account when developing the site.`,
+        thankYou: "Thank you for using DriftDataan!"
+
+    }
+}
