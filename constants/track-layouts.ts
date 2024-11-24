5977b9387f54c97f2b3ebd9f1506bb650886f5f7
Elias Koivuporras <eppu2002@gmail.com>
2024-11-24 15:06:07 +0200

    track layouts

--- constants/track-layouts.ts
+++ constants/track-layouts.ts
@@ -9,4 +9,10 @@ export const TRACK_LAYOUTS: Record<string, string> = {
     '66ac716e54d52974e6797d21': '/track-layouts/dmec_round_4_latvia_layout.png',
     '66af947b77ddd018ca852275': '/track-layouts/dmec_round_5_hungary_layout.png',
     '66af9539b2c2a169c1ea82b6': '/track-layouts/dmec_round_6_poland_layout.png',
+
+    // SM PRO
+    '6668a1c2d16394078631c58b': '/track-layouts/smpro_round1_seinajoki_layout.png',
+    '6682a4bfaaf72d395ddb55b6': '/track-layouts/smpro_round2_lapua_layout.png',
+    '669ff79654d52974e67832f3': '/track-layouts/smpro_round3_lahti_layout.png',
+    '66af95cdb2c2a169c1ea8a54': '/track-layouts/smpro_round4_hameenlinna_layout.png',
 }
