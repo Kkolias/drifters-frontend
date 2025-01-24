63bb85b009e64ec2ac6b07c2d11c909de6cfd4d2
Elias Koivuporras <eppu2002@gmail.com>
2025-01-24 21:04:52 +0200

    track layouts

--- constants/track-layouts.ts
+++ constants/track-layouts.ts
@@ -2,17 +2,25 @@
 // liitetään event id lista ja public/track-layouts kuva
 
 export const TRACK_LAYOUTS: Record<string, string> = {
-    // DMEC
-    '665ee40654115f2e9b99fb02': '/track-layouts/dmec_round_1_spain_layout.png',
-    '666e0c6faaf72d395dd9f391': '/track-layouts/dmec_round_2_ireland_layout.png',
-    '6682b02e3cb6e6f25a7a68c1': '/track-layouts/dmec_round_3_finland_layout.png',
-    '66ac716e54d52974e6797d21': '/track-layouts/dmec_round_4_latvia_layout.png',
-    '66af947b77ddd018ca852275': '/track-layouts/dmec_round_5_hungary_layout.png',
-    '66af9539b2c2a169c1ea82b6': '/track-layouts/dmec_round_6_poland_layout.png',
+  // DMEC
+  "665ee40654115f2e9b99fb02": "/track-layouts/dmec_round_1_spain_layout.png",
+  "666e0c6faaf72d395dd9f391": "/track-layouts/dmec_round_2_ireland_layout.png",
+  "6682b02e3cb6e6f25a7a68c1": "/track-layouts/dmec_round_3_finland_layout.png",
+  "66ac716e54d52974e6797d21": "/track-layouts/dmec_round_4_latvia_layout.png",
+  "66af947b77ddd018ca852275": "/track-layouts/dmec_round_5_hungary_layout.png",
+  "66af9539b2c2a169c1ea82b6": "/track-layouts/dmec_round_6_poland_layout.png",
 
-    // SM PRO
-    '6668a1c2d16394078631c58b': '/track-layouts/smpro_round1_seinajoki_layout.png',
-    '6682a4bfaaf72d395ddb55b6': '/track-layouts/smpro_round2_lapua_layout.png',
-    '669ff79654d52974e67832f3': '/track-layouts/smpro_round3_lahti_layout.png',
-    '66af95cdb2c2a169c1ea8a54': '/track-layouts/smpro_round4_hameenlinna_layout.png',
-}
+  // DMEC 2025
+  "675d6f8955a8d833809e9ed4": "/track-layouts/dmec_round_3_finland_layout.png",
+  "675d6fbaba7244bef957bee8": "/track-layouts/dmec_round_2_ireland_layout.png",
+  "675d6fe4ba7244bef957bef2": "/track-layouts/dmec_round_4_latvia_layout.png",
+  "675d7052ba7244bef957bf06": "/track-layouts/dmec_round_6_poland_layout.png",
+
+  // SM PRO
+  "6668a1c2d16394078631c58b":
+    "/track-layouts/smpro_round1_seinajoki_layout.png",
+  "6682a4bfaaf72d395ddb55b6": "/track-layouts/smpro_round2_lapua_layout.png",
+  "669ff79654d52974e67832f3": "/track-layouts/smpro_round3_lahti_layout.png",
+  "66af95cdb2c2a169c1ea8a54":
+    "/track-layouts/smpro_round4_hameenlinna_layout.png",
+};
