62f475fc84cae425c605ec7437bf0cd2d6b5763c
Elias Koivuporras <elias.koivuporras@gmail.com>
2025-06-05 20:14:30 +0300

    socket debug testi

--- plugins/socket.io.ts
+++ plugins/socket.io.ts
@@ -1,18 +1,20 @@
 import { io, Socket } from "socket.io-client";
 import { API_URL } from "~/constants/api-url.const";
 
+// const API_URL = "https://drifters-api-v2.fly.dev"
+
 export default defineNuxtPlugin((nuxtApp) => {
   const socket: Socket = io(API_URL, {
-    transports: ["websocket"],
+    transports: ["websocket", "polling"],
   }); // tai oma API-osoitteesi
 
-//   socket.on("connect", () => {
-//     console.log("✅ Socket.io connected:", socket.id);
-//   });
+  socket.on("connect", () => {
+    console.log("✅ Socket.io connected:", socket.id);
+  });
 
-//   socket.on("disconnect", () => {
-//     console.log("❌ Socket.io disconnected");
-//   });
+  socket.on("disconnect", () => {
+    console.log("❌ Socket.io disconnected");
+  });
 
   // Tuo socket käyttöön appin sisällä (esim. this.$socket tai useNuxtApp().$socket)
   nuxtApp.provide("socket", socket);
